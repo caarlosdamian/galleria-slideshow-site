9042ae8fde8e81cf8e0c63ebf38abf66589173bf
caarlosdamian <dapecarlos@gmail.com>
2024-01-02 13:45:09 -0600

    feat(page): added footer

--- app/(root)/paint/[id]/page.tsx
+++ app/(root)/paint/[id]/page.tsx
@@ -3,7 +3,11 @@ import { PaintI } from '@/types';
 
 const PaintDetails = async ({ params }: { params: { id: string } }) => {
   const paintsInfo: PaintI[] = await getData(`/api/paints/${params.id}`);
-  return <div></div>;
+  return <div>
+    <div className=""></div>
+    <div className=""></div>
+    <footer className="bg-red-500 w-full h-8"></footer>
+  </div>;
 };
 
 export default PaintDetails;
