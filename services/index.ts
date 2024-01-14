4de1ba49a2057f4884593626feb60154d2d59551
caarlosdamian <dapecarlos@gmail.com>
2024-01-14 14:03:21 -0600

    ix(deploy): fix deployment

--- services/index.ts
+++ services/index.ts
@@ -1,9 +1,12 @@
 export const getData = async (url: string) => {
-  try {
-    const res = await fetch(`${process.env.NEXT_API_ENDPOINT || 'http://localhost:3000' }${url}`);
-    const body = await res.json();
-    return body;
-  } catch (error) {
-    console.log(error);
+  if (process.env.NEXT_API_ENDPOINT) {
+    try {
+      const res = await fetch(`${process.env.NEXT_API_ENDPOINT}${url}`);
+      const body = await res.json();
+      return body;
+    } catch (error) {
+      console.log(error);
+    }
   }
+  return [];
 };
